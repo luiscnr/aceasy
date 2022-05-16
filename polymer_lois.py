dafb759411a270713b5ea1fb9c18de4214b965e1
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-05-16 10:24:42 +0200

    Added multiprocessing option

--- polymer_lois.py
+++ polymer_lois.py
@@ -5,17 +5,51 @@ import sys
 
 class POLYMER:
     def __init__(self, fconfig, verbose):
+        '''
+        Polymer options (From Polymer Main)
+        - multiprocessing: number of threads to use for processing (int)
+        N = 0: single thread (multiprocessing disactivated)
+        N != 0: use multiple threads, with
+        N < 0: use as many threads as there are CPUs on local machine
+        - normalize: select water reflectance normalization
+           * no geometry nor wavelength normalization (0)
+           * apply normalization of the water reflectance at nadir-nadir (1)
+           * apply wavelength normalization for MERIS and OLCI (2)
+           * apply both (3)
+        '''
         self.verbose = verbose
         if fconfig is None:
             fconfig = 'aceasy_config.ini'
 
         self.polymer_path = None
+
+
+
+        self.extraoptions = {
+            'multiprocessing':{
+                'value':0,
+                'apply':False
+            },
+            'normalize':{
+                'value':3,
+                'apply': False
+            }
+        }
         if os.path.exists(fconfig):
             options = configparser.ConfigParser()
             options.read(fconfig)
             if options.has_section('POLYMER'):
                 if options.has_option('POLYMER', 'polymer_path'):
                     self.polymer_path = options['POLYMER']['polymer_path']
+                if options.has_option('POLYMER','multiprocessing'):
+                    self.extraoptions['multiprocessing']['value'] = int(options['POLYMER'] ['multiprocessing'])
+                    self.extraoptions['multiprocessing']['apply'] = True
+
+                if options.has_option('POLYMER','normalize'):
+                    self.extraoptions['normalize']['value'] = int(options['POLYMER'] ['normalize'])
+                    self.extraoptions['normalize']['apply'] = True
+
+
 
     def check_runac(self):
         if self.polymer_path is None:
@@ -31,8 +65,12 @@ class POLYMER:
     def run_process(self, prod_path, output_dir):
         prod_name = prod_path.split('/')[-1]
         if prod_name.endswith('.SEN3') and os.path.isdir(prod_path):
-            output_name = prod_name[0:-5] + '_POLYMER.nc'
-            output_path = os.path.join(output_dir, output_name)
+            if os.path.isdir(output_dir):
+                output_name = prod_name[0:-5] + '_POLYMER.nc'
+                output_path = os.path.join(output_dir, output_name)
+            else:
+                output_name = os.path.basename(output_dir)
+                output_path = output_dir
         else:
             print(f'[ERROR] Product {prod_name} is not a correct *.SEN3 directory')
             return False
@@ -45,7 +83,13 @@ class POLYMER:
         sys.path.append(self.polymer_path)
         from polymer.main import run_atm_corr, Level1, Level2
         from polymer.level2_nc import Level2_NETCDF
-        res = run_atm_corr(Level1(prod_path),Level2(filename = output_path,fmt = 'netcdf4'))
+        params = {}
+        for key in self.extraoptions:
+            if self.extraoptions[key]['apply']:
+                params[key] = self.extraoptions[key]['value']
+
+        print(params)
+        res = run_atm_corr(Level1(prod_path),Level2(filename = output_path,fmt = 'netcdf4'),**params)
         if isinstance(res,Level2_NETCDF) and os.path.exists(output_path):
             if self.verbose:
                 print(f'[INFO] Polymer completed. Output file name: {output_name}')
