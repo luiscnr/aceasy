f25a7db982734aaf1c2e7d74c750043188b3dc89
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-11-15 18:19:08 +0100

    Added Bal All

--- baltic_all.py
+++ baltic_all.py
@@ -16,17 +16,17 @@ class BALTIC_ALL():
             if name.endswith('POLYMER_MLP.nc'):
                 fname = os.path.join(prod_path, name)
                 cmd = f'python {self.codepath}s3olci_bal_reproject_202211.py {fname} -c {self.codepath}s3olci_202211.yaml -od {output_dir} -v'
-                print(cmd)
+                self.launch_cmd(cmd)
         # Mosacking
         yearstr = prod_path.split('/')[-2]
         jjjstr = prod_path.split('/')[-1]
         namea = f'Oa{yearstr}{jjjstr}'
         cmd = f'python {self.codepath}s3olcimosaic_202211.py -v {output_dir}/{namea}??????--bal-fr.nc -od {output_dir} -of {namea}--bal-fr.nc'
-        print(cmd)
+        self.launch_cmd(cmd)
 
         # Splitting
         cmd = f'python {self.codepath}s3olcisplit_202211.py -v --qi --lowerleft {output_dir}/{namea}--bal-fr.nc -c {self.codepath}s3olci_202211.yaml -od {output_dir}'
-        print(cmd)
+        self.launch_cmd(cmd)
 
         # Changing name
         name_main = f'{namea}--bal-fr.nc'
@@ -39,11 +39,14 @@ class BALTIC_ALL():
                 fin = os.path.join(output_dir,name)
                 fout = os.path.join(output_dir,name_out)
                 cmd = f'mv {fin} {fout}'
-                if self.verbose:
-                    print(f'CMD: {cmd}')
-                prog = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-                out, err = prog.communicate()
-                if err:
-                    print(err)
+                self.launch_cmd(cmd)
+
+    def launch_cmd(self,cmd):
+        if self.verbose:
+            print(f'CMD: {cmd}')
+        prog = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+        out, err = prog.communicate()
+        if err:
+            print(err)
 
 
