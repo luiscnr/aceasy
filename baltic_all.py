20b21df58e2f85989126fae964cbd97ed96b4e3f
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2023-01-16 16:51:29 +0100

    Added option copy month

--- baltic_all.py
+++ baltic_all.py
@@ -3,18 +3,21 @@ import subprocess
 import configparser
 from datetime import datetime as dt
 
+
 class BALTIC_ALL():
     def __init__(self, fconfig, verbose):
         self.verbose = verbose
         if fconfig is None:
             fconfig = 'aceasy_config.ini'
-        self.codepath = '/home/gosuser/Processing/OC_PROC_EIS202207_NRTNASA_EDS/s3olciProcessing/'#default
+        self.codepath = '/home/gosuser/Processing/OC_PROC_EIS202207_NRTNASA_EDS/s3olciProcessing/'  # default
         self.onlyreformat = False
+        self.onlycopymonth = False
         self.domerge = False
         self.doresampling = True
         self.domosaic = True
         self.dosplit = True
         self.dos3b = True
+
         if os.path.exists(fconfig):
             options = configparser.ConfigParser()
             options.read(fconfig)
@@ -26,44 +29,56 @@ class BALTIC_ALL():
 
                 if options.has_option('BALALL', 'only_reformat'):
                     onlyr = options['BALALL']['only_reformat']
-                    if onlyr.lower()=='true':
+                    if onlyr.lower() == 'true':
                         self.onlyreformat = True
 
                 if options.has_option('BALALL', 'do_resampling'):
                     onlyr = options['BALALL']['do_resampling']
-                    if onlyr.lower()=='false':
+                    if onlyr.lower() == 'false':
                         self.doresampling = False
 
                 if options.has_option('BALALL', 'do_mosaic'):
                     onlyr = options['BALALL']['do_mosaic']
-                    if onlyr.lower()=='false':
+                    if onlyr.lower() == 'false':
                         self.domosaic = False
 
                 if options.has_option('BALALL', 'do_split'):
                     onlyr = options['BALALL']['do_split']
-                    if onlyr.lower()=='false':
+                    if onlyr.lower() == 'false':
                         self.dosplit = False
 
                 if options.has_option('BALALL', 'do_s3b'):
                     onlyr = options['BALALL']['do_s3b']
-                    if onlyr.lower()=='false':
+                    if onlyr.lower() == 'false':
                         self.dos3b = False
+
                 if options.has_option('BALALL', 'do_merge'):
                     onlyr = options['BALALL']['do_merge']
-                    if onlyr.lower()=='true':
+                    if onlyr.lower() == 'true':
                         self.domerge = True
 
+                if options.has_option('BALALL', 'copy_formonth'):
+                    onlyr = options['BALALL']['copy_formonth']
+                    if onlyr.lower() == 'true':
+                        self.onlycopymonth = True
 
     def check_runac(self):
         # NO IMPLEMENTED
         return True
 
     def run_process(self, prod_path, output_dir):
+
+        # Only copy before processing month
+        if self.onlycopymonth:
+            self.copy_month(prod_path, output_dir)
+            return
+        # Only reformat
         if self.onlyreformat:
             if self.domerge:
-                self.run_merge(prod_path,output_dir)
-            self.run_reformat(prod_path,output_dir)
+                self.run_merge(prod_path, output_dir)
+            self.run_reformat(prod_path, output_dir)
             return
+        # Merge
         if self.domerge:
             self.run_merge(prod_path, output_dir)
             return
@@ -78,7 +93,7 @@ class BALTIC_ALL():
                         expected_oname = f'Oa{sat_time_str}--bal-fr.nc'
                     elif name.startswith('S3B'):
                         expected_oname = f'Ob{sat_time_str}--bal-fr.nc'
-                    expected_ofile = os.path.join(output_dir,expected_oname)
+                    expected_ofile = os.path.join(output_dir, expected_oname)
                     if os.path.exists(expected_ofile):
                         print(f'[INFO] Output file {expected_ofile} already exist. Continue...')
                         continue
@@ -90,7 +105,7 @@ class BALTIC_ALL():
             yearstr = prod_path.split('/')[-2]
             jjjstr = prod_path.split('/')[-1]
             namea = f'Oa{yearstr}{jjjstr}'
-            expected_ofile = os.path.join(output_dir,f'{namea}--bal-fr.nc')
+            expected_ofile = os.path.join(output_dir, f'{namea}--bal-fr.nc')
             if os.path.exists(expected_ofile):
                 print(f'[INFO] Output file {expected_ofile} already exist. Continue...')
             else:
@@ -114,11 +129,11 @@ class BALTIC_ALL():
             prename_end = f'O{yearstr}{jjjstr}-'
             nfiles = 0
             for name in os.listdir(output_dir):
-                if name==name_main_end:
+                if name == name_main_end:
                     continue
                 if name.startswith(prename_end):
-                    nfiles = nfiles +1
-            if nfiles==28:
+                    nfiles = nfiles + 1
+            if nfiles == 28:
                 print('[INFO] Files are already available. Completed')
                 return
 
@@ -134,16 +149,16 @@ class BALTIC_ALL():
                 name_main = f'{namea}--bal-fr.nc'
                 prename = f'{namea}-'
                 for name in os.listdir(output_dir):
-                    if name==name_main:
+                    if name == name_main:
                         continue
                     if name.startswith(prename):
-                        name_out = name.replace('Oa','O')
-                        fin = os.path.join(output_dir,name)
-                        fout = os.path.join(output_dir,name_out)
+                        name_out = name.replace('Oa', 'O')
+                        fin = os.path.join(output_dir, name)
+                        fout = os.path.join(output_dir, name_out)
                         cmd = f'mv {fin} {fout}'
                         self.launch_cmd(cmd)
 
-    def run_reformat(self,prod_path,output_dir):
+    def run_reformat(self, prod_path, output_dir):
         yearstr = prod_path.split('/')[-2]
         jjjstr = prod_path.split('/')[-1]
         name_main_end = f'O{yearstr}{jjjstr}--bal-fr.nc'
@@ -167,25 +182,24 @@ class BALTIC_ALL():
         cmd = f'{self.codepath}reformatting_file_cmems2_202211.sh -res FR -m MY -r BAL -f D -p transp -path {output_dir}'
         self.launch_cmd(cmd)
 
-    def run_merge(self,prod_path,output_dir):
+    def run_merge(self, prod_path, output_dir):
         yearstr = prod_path.split('/')[-2]
         jjjstr = prod_path.split('/')[-1]
-        filesa,nfilesa = self.check_nfiles('Oa',prod_path,output_dir)
-        filesb,nfilesb = self.check_nfiles('Ob',prod_path,output_dir)
+        filesa, nfilesa = self.check_nfiles('Oa', prod_path, output_dir)
+        filesb, nfilesb = self.check_nfiles('Ob', prod_path, output_dir)
 
-        if nfilesa==28 and nfilesb==0:
+        if nfilesa == 28 and nfilesb == 0:
             for filea in filesa:
-                filea_o = filea.replace('Oa20','O20')
+                filea_o = filea.replace('Oa20', 'O20')
                 cmd = f'cp -a {filea} {filea_o}'
                 self.launch_cmd(cmd)
             return
-        if nfilesb==28 and nfilesa==0:
+        if nfilesb == 28 and nfilesa == 0:
             for fileb in filesb:
-                fileb_o = fileb.replace('Ob20','O20')
+                fileb_o = fileb.replace('Ob20', 'O20')
                 cmd = f'cp -a {fileb} {fileb_o}'
                 self.launch_cmd(cmd)
             return
-
 
         if nfilesa < 28:
             print('[INFO] Files S3A are not available. Skyping merge...')
@@ -194,21 +208,21 @@ class BALTIC_ALL():
             print('[INFO] Files S3B are not available. Skyping merge...')
             return
 
-        #output dir for making merge
-        dirbase='/DataArchive/OC/OLCI/dailybal202211'
-        output_dir_base = self.get_output_directory(dirbase,yearstr,jjjstr)
+        # output dir for making merge
+        dirbase = '/DataArchive/OC/OLCI/dailybal202211'
+        output_dir_base = self.get_output_directory(dirbase, yearstr, jjjstr)
         for filea in filesa:
             cmd = f'cp -a {filea} {output_dir_base}'
             self.launch_cmd(cmd)
-        for fileb  in filesb:
+        for fileb in filesb:
             cmd = f'cp -a {fileb} {output_dir_base}'
             self.launch_cmd(cmd)
-        #mergin
-        datehere = dt.strptime(f'{yearstr}{jjjstr}','%Y%j')
+        # mergin
+        datehere = dt.strptime(f'{yearstr}{jjjstr}', '%Y%j')
         dateheres = datehere.strftime('%Y-%m-%d')
         cmd = f'sh {self.codepath}make_merge_olci_202211.sh -d {dateheres} -a bal -r fr -v'
         self.launch_cmd(cmd)
-        #copying again
+        # copying again
         files, nfiles = self.check_nfiles('O', prod_path, output_dir_base)
         print(nfiles)
         if nfiles < 27:
@@ -219,7 +233,7 @@ class BALTIC_ALL():
             self.launch_cmd(cmd)
         print('COMPLETED')
 
-    def get_output_directory(self,output_path,year_str,day_str):
+    def get_output_directory(self, output_path, year_str, day_str):
         output_path_year = os.path.join(output_path, year_str)
         if not os.path.exists(output_path_year):
             st = os.stat(output_path)
@@ -236,7 +250,25 @@ class BALTIC_ALL():
 
         return output_path_jday
 
-    def check_nfiles(self,ref,prod_path,output_dir):
+    def copy_month(self, input_path, output_path):
+        jjj = input_path.split('/')[-1]
+        yyyy = input_path.split('/')[-2]
+
+        namechl = f'O{yyyy}{jjj}-chl-bal-fr.nc'
+        filechl = os.path.join(input_path, namechl)
+        filechlout = os.path.join(output_path, namechl)
+        if os.path.exists(filechl):
+            cmd = f'cp -a {filechl} {filechlout}'
+            self.launch_cmd(cmd)
+
+        namekd = f'O{yyyy}{jjj}-kd490-bal-fr.nc'
+        filekd = os.path.join(input_path, namekd)
+        filekdout = os.path.join(output_path, namekd)
+        if os.path.exists(filekd):
+            cmd = f'cp -a {filekd} {filekdout}'
+            self.launch_cmd(cmd)
+
+    def check_nfiles(self, ref, prod_path, output_dir):
         yearstr = prod_path.split('/')[-2]
         jjjstr = prod_path.split('/')[-1]
         name_main_end = f'{ref}{yearstr}{jjjstr}--bal-fr.nc'
@@ -247,11 +279,11 @@ class BALTIC_ALL():
             if name == name_main_end:
                 continue
             if name.startswith(prename_end):
-                files.append(os.path.join(output_dir,name))
+                files.append(os.path.join(output_dir, name))
                 nfiles = nfiles + 1
-        return files,nfiles
+        return files, nfiles
 
-    def launch_cmd(self,cmd):
+    def launch_cmd(self, cmd):
         if self.verbose:
             print(f'CMD: {cmd}')
         prog = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
@@ -259,7 +291,7 @@ class BALTIC_ALL():
         if err:
             print(err)
 
-    def get_sat_time_from_fname(self,fname):
+    def get_sat_time_from_fname(self, fname):
         val_list = fname.split('_')
         sat_time = None
         for v in val_list:
