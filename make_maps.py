3df88a5004093d8d5ab7c1cddc6799b0e14d7806
luiscnr <lois@LAPTOP-SU04997U>
2024-07-01 14:01:07 +0200

    Improved method for getting image differences

--- make_maps.py
+++ make_maps.py
@@ -707,84 +707,82 @@ def launch_single_map(dataset, output_path, dateherestr):
     weight_arrays = [weight_mlp3b, weight_mlp4b, weight_mlp5b]
     titles = ['Weight CDF MLP3B','Weight CDF MLP4B','Weight CDF MLP5B']
     ##weight arrays
-    for idx in range(len(weight_arrays)):
-        fig, ax = start_full_figure()
-        array = weight_arrays[idx]
-        h = ax.pcolormesh(lon_array, lat_array,array,cmap = mpl.colormaps['jet'],vmin=0,vmax=1)
-        cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
-        cbar.ax.tick_params(labelsize=15)
-        cbar.set_label(label=f'Weight', size=15)
-        title = titles[idx]
-        if dateherestr is not None:
-            title = f'{title} - {dateherestr}'
-        ax.set_title(title, fontsize=20)
-        name = title.replace(' ','_')
-        output_path_here = os.path.join(os.path.dirname(output_path), f'Img_{name}_.png')
-        fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
-        plt.close(fig)
+    # for idx in range(len(weight_arrays)):
+    #     fig, ax = start_full_figure()
+    #     array = weight_arrays[idx]
+    #     h = ax.pcolormesh(lon_array, lat_array,array,cmap = mpl.colormaps['jet'],vmin=0,vmax=1)
+    #     cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
+    #     cbar.ax.tick_params(labelsize=15)
+    #     cbar.set_label(label=f'Weight', size=15)
+    #     title = titles[idx]
+    #     if dateherestr is not None:
+    #         title = f'{title} - {dateherestr}'
+    #     ax.set_title(title, fontsize=20)
+    #     name = title.replace(' ','_')
+    #     output_path_here = os.path.join(os.path.dirname(output_path), f'Img_{name}_.png')
+    #     fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    #     plt.close(fig)
 
 
     ##flag multiple
-    fig, ax = start_full_figure()
-    bounds = [1, 2, 4, 6, 8, 10, 12, 14, 15]
-    norm = colors.BoundaryNorm(boundaries=bounds, ncolors=9)
-    h = ax.pcolormesh(lon_array, lat_array, cdf_flag_multiple,norm = norm, cmap = mpl.colormaps['Set1'])
-    cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03,format="$%.2f$")
-    cbar.ax.tick_params(labelsize=15)
-    cbar.set_label(label=f'CDF Flag Multiple', size=15)
-    title = f'CDF Flag Multiple'
-    if dateherestr is not None:
-        title = f'{title} - {dateherestr}'
-    ax.set_title(title, fontsize=20)
-    output_path_here = os.path.join(os.path.dirname(output_path),f'Img_FlagMultiple_{dateherestr}.png')
-    fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
-    plt.close(fig)
+    # fig, ax = start_full_figure()
+    # bounds = [1, 2, 4, 6, 8, 10, 12, 14, 15]
+    # norm = colors.BoundaryNorm(boundaries=bounds, ncolors=9)
+    # h = ax.pcolormesh(lon_array, lat_array, cdf_flag_multiple,norm = norm, cmap = mpl.colormaps['Set1'])
+    # cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03,format="$%.2f$")
+    # cbar.ax.tick_params(labelsize=15)
+    # cbar.set_label(label=f'CDF Flag Multiple', size=15)
+    # title = f'CDF Flag Multiple'
+    # if dateherestr is not None:
+    #     title = f'{title} - {dateherestr}'
+    # ax.set_title(title, fontsize=20)
+    # output_path_here = os.path.join(os.path.dirname(output_path),f'Img_FlagMultiple_{dateherestr}.png')
+    # fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    # plt.close(fig)
 
     ##flag_cyano
-    rrs555 = dataset.variables['RRS560'][:]
-    rrs670 = dataset.variables['RRS665'][:]
-    rrs555 = rrs555 * np.pi
-    rrs670 = rrs670 * np.pi
-    mask_cyano = np.zeros(rrs670.shape)
-    mask_cyano[rrs555 > 4.25e-3] = mask_cyano[rrs555 > 4.25e-3] + 1
-    mask_cyano[rrs670 > 1.22e-3] = mask_cyano[rrs670 > 1.22e-3] + 2
-    mask_cyano = np.ma.array(mask_cyano,mask = cdf_flag_multiple.mask)
-    fig, ax = start_full_figure()
-    bounds = [0, 1, 2, 3, 4]
-    norm = colors.BoundaryNorm(boundaries=bounds, ncolors=5)
-    newcolors = ['blue','red','green','purple']
-    newcmp = ListedColormap(newcolors)
-    h = ax.pcolormesh(lon_array, lat_array, mask_cyano, norm=norm, cmap=newcmp)
-    cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
-    cbar.ax.tick_params(labelsize=15)
-    cbar.set_label(label=f'Flag Cyano', size=15)
-    title = f'Cyanobacterial flag'
-    if dateherestr is not None:
-        title = f'{title} - {dateherestr}'
-    ax.set_title(title, fontsize=20)
-    output_path_here = os.path.join(os.path.dirname(output_path), f'Img_FlagCyano_{dateherestr}.png')
-    fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
-    plt.close(fig)
+    # rrs555 = dataset.variables['RRS560'][:]
+    # rrs670 = dataset.variables['RRS665'][:]
+    # rrs555 = rrs555 * np.pi
+    # rrs670 = rrs670 * np.pi
+    # mask_cyano = np.zeros(rrs670.shape)
+    # mask_cyano[rrs555 > 4.25e-3] = mask_cyano[rrs555 > 4.25e-3] + 1
+    # mask_cyano[rrs670 > 1.22e-3] = mask_cyano[rrs670 > 1.22e-3] + 2
+    # mask_cyano = np.ma.array(mask_cyano,mask = cdf_flag_multiple.mask)
+    # fig, ax = start_full_figure()
+    # bounds = [0, 1, 2, 3, 4]
+    # norm = colors.BoundaryNorm(boundaries=bounds, ncolors=5)
+    # newcolors = ['blue','red','green','purple']
+    # newcmp = ListedColormap(newcolors)
+    # h = ax.pcolormesh(lon_array, lat_array, mask_cyano, norm=norm, cmap=newcmp)
+    # cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
+    # cbar.ax.tick_params(labelsize=15)
+    # cbar.set_label(label=f'Flag Cyano', size=15)
+    # title = f'Cyanobacterial flag'
+    # if dateherestr is not None:
+    #     title = f'{title} - {dateherestr}'
+    # ax.set_title(title, fontsize=20)
+    # output_path_here = os.path.join(os.path.dirname(output_path), f'Img_FlagCyano_{dateherestr}.png')
+    # fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    # plt.close(fig)
 
     dataset.close()
 
     ##multiple plot
-    fig, ax = plt.subplots(2, 3, figsize=(15, 6), frameon=True, gridspec_kw={'wspace': 0, 'hspace': 0})
-    from matplotlib import image as img
-    files_img = [[f'Img_Chla_{dateherestr}.png',f'Img_FlagMultiple_{dateherestr}.png',f'Img_FlagCyano_{dateherestr}.png'],
-                 [f'Img_Weight_CDF_MLP3B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP4B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP5B_-_{dateherestr}_.png']]
-    #print(files_img)
-
-    for irow in range(2):
-        for icol in range(3):
-            file_img = os.path.join(os.path.dirname(output_path),files_img[irow][icol])
-            image = img.imread(file_img)
-            ax[irow, icol].imshow(image)
-            ax[irow, icol].set_xticks([])
-            ax[irow, icol].set_yticks([])
-    fig.tight_layout()
-    file_out = os.path.join(os.path.dirname(output_path), f'Img_DayAll_{dateherestr}.png')
-    plt.savefig(file_out, dpi=300, bbox_inches='tight', facecolor='white')
+    # fig, ax = plt.subplots(2, 3, figsize=(15, 6), frameon=True, gridspec_kw={'wspace': 0, 'hspace': 0})
+    # from matplotlib import image as img
+    # files_img = [[f'Img_Chla_{dateherestr}.png',f'Img_FlagMultiple_{dateherestr}.png',f'Img_FlagCyano_{dateherestr}.png'],
+    #              [f'Img_Weight_CDF_MLP3B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP4B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP5B_-_{dateherestr}_.png']]
+    # for irow in range(2):
+    #     for icol in range(3):
+    #         file_img = os.path.join(os.path.dirname(output_path),files_img[irow][icol])
+    #         image = img.imread(file_img)
+    #         ax[irow, icol].imshow(image)
+    #         ax[irow, icol].set_xticks([])
+    #         ax[irow, icol].set_yticks([])
+    # fig.tight_layout()
+    # file_out = os.path.join(os.path.dirname(output_path), f'Img_DayAll_{dateherestr}.png')
+    # plt.savefig(file_out, dpi=300, bbox_inches='tight', facecolor='white')
 
 
     print(f'[INFO] Completed')
