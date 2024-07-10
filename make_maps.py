87ffda406a9c0c93f5e7fcd20b4d7122fdbf2c62
luiscnr <lois@LAPTOP-SU04997U>
2024-07-10 18:57:35 +0200

    Make maps olci #1

--- make_maps.py
+++ make_maps.py
@@ -522,32 +522,41 @@ def main():
             output_path = args.output_path
         else:
             output_path = input_path.replace('.nc', '.png')
-        dateherestr = get_date_here_from_file_name(input_path)
-        if dateherestr is None:
+        dateherestr, type = get_date_here_from_file_name(input_path)
+        if type=='diff': ##Daily diff
             name = os.path.basename(input_path)
             datestr = name[5:12]
             dateherestr = dt.strptime(datestr, '%Y%j').strftime('%Y%m%d')
-            launch_single_map_temp(dataset, output_path, dateherestr)
-        else:
-            launch_single_map(dataset, output_path, dateherestr)
-
+            launch_single_map_daily_diff(dataset, output_path, dateherestr)
+        elif type=='cci':
+            launch_single_map_cci(dataset, output_path, dateherestr)
+        elif type=='olci':
+            launch_single_map_olci(dataset, output_path, dateherestr)
 
 def launch_multiple_maps(input_dir):
     for name in os.listdir(input_dir):
         if not name.endswith('.nc') or name.startswith('Coverage'):
             continue
         input_path = os.path.join(input_dir, name)
-        date_here_str = get_date_here_from_file_name(input_path)
+        date_here_str,type = get_date_here_from_file_name(input_path)
 
-        if date_here_str is not None:
+        if type=='cci':
             try:
                 print(f'[INFO] Input path: {input_path}')
                 dataset = Dataset(input_path)
             except:
                 continue
             output_path = input_path.replace('.nc', '.png')
-            launch_single_map(dataset, output_path, date_here_str)
-        else:
+            launch_single_map_cci(dataset, output_path, date_here_str)
+        elif type == 'olci':
+            try:
+                print(f'[INFO] Input path: {input_path}')
+                dataset = Dataset(input_path)
+            except:
+                continue
+            output_path = input_path.replace('.nc', '.png')
+            launch_single_map_olci(dataset, output_path, date_here_str)
+        elif type=='diff':
             if not name.endswith('.nc'):
                 continue
             input_path = os.path.join(input_dir, name)
@@ -560,10 +569,11 @@ def launch_multiple_maps(input_dir):
             dateherestr = dt.strptime(datestr, '%Y%j').strftime('%Y%m%d')
             output_path = input_path.replace('.nc', '.png')
             # print(output_path)
-            launch_single_map_temp(dataset, output_path, dateherestr)
+            launch_single_map_daily_diff(dataset, output_path, dateherestr)
 
 
-def launch_single_map_temp(dataset, output_path, dateherestr):
+
+def launch_single_map_daily_diff(dataset, output_path, dateherestr):
     print(f'[INFO] Output path: {output_path}')
 
     lat_array = dataset.variables['latitude'][:]
@@ -665,8 +675,158 @@ def launch_single_map_temp(dataset, output_path, dateherestr):
     dataset.close()
     plt.close(fig)
 
+def launch_single_map_olci(dataset, output_path, dateherestr):
+    print(f'[INFO] Output path: {output_path}')
 
-def launch_single_map(dataset, output_path, dateherestr):
+    lat_array = dataset.variables['latitude'][:]
+    lon_array = dataset.variables['longitude'][:]
+    data = dataset.variables['CHL'][:]
+
+    data_stats = np.ma.compressed(data)
+
+    ##chl-a- fix-range
+    fig, ax = start_full_figure()
+    h = ax.pcolormesh(lon_array, lat_array, data, norm=LogNorm(vmin=0.1, vmax=100))
+    cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
+    cbar.ax.tick_params(labelsize=15)
+    units = r'mg m$^-$$^3$'
+    cbar.set_label(label=f'CHL ({units})', size=15)
+    title = f'Chlorophyll a concentration ({units})'
+    if dateherestr is not None:
+        title = f'{title} - {dateherestr}'
+    ax.set_title(title, fontsize=20)
+    fig.savefig(output_path, dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    ##chl-a
+    fig,ax = start_full_figure()
+    min_chla = np.percentile(data_stats, 1)
+    max_chla = np.percentile(data_stats, 99)
+    h = ax.pcolormesh(lon_array,lat_array,data,norm=LogNorm(vmin=min_chla, vmax=max_chla))
+    cbar = fig.colorbar(h,cax = None, ax = ax, use_gridspec = True, fraction=0.03,format="$%.2f$")
+    cbar.ax.tick_params(labelsize=15)
+    units = r'mg m$^-$$^3$'
+    cbar.set_label(label=f'CHL ({units})', size=15)
+    title = f'Chlorophyll a concentration ({units})'
+    if dateherestr is not None:
+        title = f'{title} - {dateherestr}'
+    ax.set_title(title, fontsize=20)
+    output_path_here = os.path.join(os.path.dirname(output_path), f'Img_Chla_{dateherestr}.png')
+    fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    dataset.close()
+
+    #cdf flag
+    # cdf_mlp3b = dataset.variables['CDF_MLP3B'][:]
+    # cdf_mlp4b = dataset.variables['CDF_MLP4B'][:]
+    # cdf_mlp5b = dataset.variables['CDF_MLP5B'][:]
+    # cdf_mask_mlp3b = np.ma.where(cdf_mlp3b >= 0.001,2,0)
+    # cdf_mask_mlp4b = np.ma.where(cdf_mlp4b >= 0.001,4,0)
+    # cdf_mask_mlp5b = np.ma.where(cdf_mlp5b >= 0.001,8,0)
+    # cdf_flag_multiple = np.ma.filled(cdf_mask_mlp3b,0)+np.ma.filled(cdf_mask_mlp4b,0)+np.ma.filled(cdf_mask_mlp5b,0)
+    # cdf_flag_multiple[cdf_flag_multiple == 0] = 1
+    # cdf_flag_multiple = np.ma.array(cdf_flag_multiple,mask=(cdf_mlp3b.mask*cdf_mask_mlp4b.mask*cdf_mask_mlp5b.mask))
+    #
+    # cdf_mlp3b = np.ma.masked_less(cdf_mlp3b, 0.001)
+    # cdf_mlp4b = np.ma.masked_less(cdf_mlp4b, 0.001)
+    # cdf_mlp5b = np.ma.masked_less(cdf_mlp5b, 0.001)
+    # cdf_sum = np.ma.filled(cdf_mlp3b,0) + np.ma.filled(cdf_mlp4b,0) + np.ma.filled(cdf_mlp5b,0)
+    # weight_mlp3b = np.ma.divide(cdf_mlp3b, cdf_sum)
+    # weight_mlp4b = np.ma.divide(cdf_mlp4b, cdf_sum)
+    # weight_mlp5b = np.ma.divide(cdf_mlp5b, cdf_sum)
+    #
+    #
+    #
+    # weight_arrays = [weight_mlp3b, weight_mlp4b, weight_mlp5b]
+    # titles = ['Weight CDF MLP3B','Weight CDF MLP4B','Weight CDF MLP5B']
+    # ##weight arrays
+    # for idx in range(len(weight_arrays)):
+    #     fig, ax = start_full_figure()
+    #     array = weight_arrays[idx]
+    #     h = ax.pcolormesh(lon_array, lat_array,array,cmap = mpl.colormaps['jet'],vmin=0,vmax=1)
+    #     cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
+    #     cbar.ax.tick_params(labelsize=15)
+    #     cbar.set_label(label=f'Weight', size=15)
+    #     title = titles[idx]
+    #     if dateherestr is not None:
+    #         title = f'{title} - {dateherestr}'
+    #     ax.set_title(title, fontsize=20)
+    #     name = title.replace(' ','_')
+    #     output_path_here = os.path.join(os.path.dirname(output_path), f'Img_{name}_.png')
+    #     fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    #     plt.close(fig)
+    #
+    #
+    # #flag multiple
+    # fig, ax = start_full_figure()
+    # bounds = [1, 2, 4, 6, 8, 10, 12, 14, 15]
+    # norm = colors.BoundaryNorm(boundaries=bounds, ncolors=9)
+    # h = ax.pcolormesh(lon_array, lat_array, cdf_flag_multiple,norm = norm, cmap = mpl.colormaps['Set1'])
+    # cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03,format="$%.2f$")
+    # cbar.ax.tick_params(labelsize=15)
+    # cbar.set_label(label=f'CDF Flag Multiple', size=15)
+    # title = f'CDF Flag Multiple'
+    # if dateherestr is not None:
+    #     title = f'{title} - {dateherestr}'
+    # ax.set_title(title, fontsize=20)
+    # output_path_here = os.path.join(os.path.dirname(output_path),f'Img_FlagMultiple_{dateherestr}.png')
+    # fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    # plt.close(fig)
+    #
+    # #flag_cyano
+    # rrs555 = dataset.variables['RRS560'][:]
+    # rrs670 = dataset.variables['RRS665'][:]
+    # rrs555 = rrs555 * np.pi
+    # rrs670 = rrs670 * np.pi
+    # mask_cyano = np.zeros(rrs670.shape)
+    # mask_cyano[rrs555 > 4.25e-3] = mask_cyano[rrs555 > 4.25e-3] + 1
+    # mask_cyano[rrs670 > 1.22e-3] = mask_cyano[rrs670 > 1.22e-3] + 2
+    # mask_cyano = np.ma.array(mask_cyano,mask = cdf_flag_multiple.mask)
+    # fig, ax = start_full_figure()
+    # bounds = [0, 1, 2, 3, 4]
+    # norm = colors.BoundaryNorm(boundaries=bounds, ncolors=5)
+    # newcolors = ['blue','red','green','purple']
+    # newcmp = ListedColormap(newcolors)
+    # h = ax.pcolormesh(lon_array, lat_array, mask_cyano, norm=norm, cmap=newcmp)
+    # cbar = fig.colorbar(h, cax=None, ax=ax, use_gridspec=True, fraction=0.03, format="$%.2f$")
+    # cbar.ax.tick_params(labelsize=15)
+    # cbar.set_label(label=f'Flag Cyano', size=15)
+    # title = f'Cyanobacterial flag'
+    # if dateherestr is not None:
+    #     title = f'{title} - {dateherestr}'
+    # ax.set_title(title, fontsize=20)
+    # output_path_here = os.path.join(os.path.dirname(output_path), f'Img_FlagCyano_{dateherestr}.png')
+    # fig.savefig(output_path_here, dpi=300, bbox_inches='tight')
+    # plt.close(fig)
+    #
+    # dataset.close()
+    #
+    # ##multiple plot
+    # file_out = os.path.join(os.path.dirname(output_path), f'Img_DayAll_{dateherestr}.png')
+    # if os.path.exists(file_out):
+    #     return
+    # fig, ax = plt.subplots(2, 3, figsize=(15, 6), frameon=True, gridspec_kw={'wspace': 0, 'hspace': 0})
+    # from matplotlib import image as img
+    # files_img = [[f'Img_Chla_{dateherestr}.png',f'Img_FlagMultiple_{dateherestr}.png',f'Img_FlagCyano_{dateherestr}.png'],
+    #              [f'Img_Weight_CDF_MLP3B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP4B_-_{dateherestr}_.png',f'Img_Weight_CDF_MLP5B_-_{dateherestr}_.png']]
+    # for irow in range(2):
+    #     for icol in range(3):
+    #         file_img = os.path.join(os.path.dirname(output_path),files_img[irow][icol])
+    #         image = img.imread(file_img)
+    #         ax[irow, icol].imshow(image)
+    #         ax[irow, icol].set_xticks([])
+    #         ax[irow, icol].set_yticks([])
+    # fig.tight_layout()
+    # plt.savefig(file_out, dpi=300, bbox_inches='tight', facecolor='white')
+    # plt.close(fig)
+
+
+
+    print(f'[INFO] Completed')
+
+
+def launch_single_map_cci(dataset, output_path, dateherestr):
     print(f'[INFO] Output path: {output_path}')
 
     # lat_array = dataset.variables['latitude'][:]
@@ -815,15 +975,25 @@ def launch_single_map(dataset, output_path, dateherestr):
 
 
 def get_date_here_from_file_name(input_path):
+    type = 'diff'
     name = os.path.basename(input_path)
     date_here_str = None
     if name.startswith('M') and name.endswith('BAL202411.nc'):
         try:
             date_here = dt.strptime(name[1:8], '%Y%j')
             date_here_str = date_here.strftime('%Y-%m-%d')
+            type  = 'cci'
         except:
             pass
-    return date_here_str
+    if name.startswith('O') and name.endswith('BAL202411.nc'):
+        try:
+            date_here = dt.strptime(name[1:8], '%Y%j')
+            date_here_str = date_here.strftime('%Y-%m-%d')
+            type  = 'olci'
+        except:
+            pass
+
+    return date_here_str,type
 
 
 def start_full_figure():
