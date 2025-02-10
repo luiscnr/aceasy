d25df1b66e460023e3118489fbeae4e784fbde0f
luiscnr <lois@LAPTOP-SU04997U>
2025-02-10 12:48:38 +0100

    Added option to correct negative values using slurm

--- make_utils.py
+++ make_utils.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 parser = argparse.ArgumentParser(
     description="Obtaining information for running MDB_builder.")
 
-parser.add_argument('-m', "--mode", help='Mode option', choices=["check_neg_olci_values", "correct_neg_olci_values"],
+parser.add_argument('-m', "--mode", help='Mode option', choices=["check_neg_olci_values", "correct_neg_olci_values","correct_neg_olci_values_slurm"],
                     required=True)
 parser.add_argument('-i', "--input_path", help="Input path.")
 parser.add_argument('-o', "--output", help="Output file.")
@@ -56,6 +56,109 @@ def check_neg_olci_values(input_path, output_file, start_date, end_date, region)
         work_date = work_date + timedelta(hours=24)
     fw.close()
 
+def correct_neg_olci_values(input_path, start_date, end_date, region):
+    work_date = start_date
+    bands = ['400', '412_5', '442_5', '490', '510', '560', '620', '665', '673_75', '681_25', '708_75', '753_75',
+             '778_75', '865', '885', '1020']
+    while work_date <= end_date:
+        yyyy = work_date.strftime('%Y')
+        jjj = work_date.strftime('%j')
+        yyyymmdd = work_date.strftime('%Y-%m-%d')
+        print(f'[INFO] Work date: {yyyymmdd}')
+        for band in bands:
+            file_a = os.path.join(input_path, yyyy, jjj, f'Oa{yyyy}{jjj}-rrs{band}-{region.lower()}-fr.nc')
+            file_b = os.path.join(input_path, yyyy, jjj, f'Ob{yyyy}{jjj}-rrs{band}-{region.lower()}-fr.nc')
+            file_o = os.path.join(input_path, yyyy, jjj, f'O{yyyy}{jjj}-rrs{band}-{region.lower()}-fr.nc')
+            file_out = f'{file_o[:-3]}_temp.nc'
+            neg_band = f'RRS{band}'
+            print(f'[INFO]--> File: {file_o}')
+            correct_neg_olci_values_impl(file_o,file_out,file_a,file_b,neg_band)
+            os.rename(file_out,file_o)
+
+        work_date = work_date + timedelta(hours=24)
+
+def correct_neg_olci_values_impl(input_file,output_file,file_a,file_b,neg_band):
+    input_dataset = Dataset(input_file)
+    ncout = Dataset(output_file, 'w', format='NETCDF4')
+
+
+    #new_array = input_array.copy()
+    dataset_a = Dataset(file_a)
+    array_a = dataset_a.variables[neg_band][:]
+    dataset_a.close()
+    dataset_b = Dataset(file_b)
+    array_b = dataset_b.variables[neg_band][:]
+    dataset_b.close()
+
+    new_array = np.ma.mean(np.ma.concatenate([array_a,array_b],axis=0),axis=0)
+
+
+    ##TESTING CHECK CODE
+    # input_array = np.ma.squeeze(input_dataset.variables[neg_band][:])
+    # array_a = np.ma.squeeze(array_a)
+    # array_b = np.ma.squeeze(array_b)
+    # indices = np.where(np.logical_and(array_a>0,array_b>0))
+    # prev_valid = input_array[indices]
+    # new_valid = new_array[indices]
+    # ratio = prev_valid/new_valid
+    # print(np.ma.min(ratio),np.ma.max(ratio))
+    # print('old', np.ma.min(input_array),np.ma.max(input_array))
+    # print('new', np.ma.min(new_array), np.ma.max(new_array))
+
+
+
+
+    # copy global attributes all at once via dictionary
+    ncout.setncatts(input_dataset.__dict__)
+
+    # copy dimensions
+    for name, dimension in input_dataset.dimensions.items():
+        ncout.createDimension(
+            name, (len(dimension) if not dimension.isunlimited() else None))
+
+    for name, variable in input_dataset.variables.items():
+        fill_value = None
+        if '_FillValue' in list(variable.ncattrs()):
+            fill_value = variable._FillValue
+
+        ncout.createVariable(name, variable.datatype, variable.dimensions, fill_value=fill_value, zlib=True,
+                             complevel=6)
+        # copy variable attributes all at once via dictionary
+        ncout[name].setncatts(input_dataset[name].__dict__)
+        if name == neg_band:
+            ncout[name][:] = new_array[:]
+        else:
+            ncout[name][:] = input_dataset[name][:]
+
+    ncout.close()
+    input_dataset.close()
+
+
+def correct_neg_olci_values_slurm(dir_log,start_date, end_date, region):
+    work_path = '/home/gosuser/Processing/gos-oc-processingchains_v202411/s3olciProcessing'
+    line_py = f'python {work_path}/make_merge_olci_202311.new.py -d DATE -a {region.lower()} -p RRS -v'
+    work_date = start_date
+    while work_date<=end_date:
+        fslurm = os.path.join(dir_log,f'LaunchMergeOLCI_{work_date.strftime("%Y%m%d")}.slurm')
+        fw = open(fslurm,'w')
+        fw.write('#!/bin/bash')
+        add_new_line(fw,'#SBATCH --nodes=1')
+        add_new_line(fw,'#SBATCH --ntasks=1')
+        add_new_line(fw, f'#SBATCH --output {fslurm.replace(".slurm",".log")}')
+        add_new_line(fw,'#SBATCH -p octac_rep')
+        add_new_line(fw,'#SBATCH --mail-type=BEGIN,END,FAIL')
+        add_new_line(fw,'#SBATCH --mail-user=luis.gonzalezvilas@artov.ismar.cnr.it,lorenzo.amodio@artov.ismar.cnr.it')
+        add_new_line(fw,'')
+        add_new_line(fw,'source /home/gosuser/load_miniconda3.source')
+        add_new_line(fw,'conda activate op_proc_202211v2')
+        add_new_line(fw,'')
+        line_py = line_py.replace("DATE",work_date.strftime('%Y-%m-%d'))
+        add_new_line(fw,line_py)
+        work_date = work_date + timedelta(hours=24)
+
+def add_new_line(fw,line):
+    fw.write('\n')
+    fw.write(line)
 def get_min_rrs_value(file,band):
     dataset = Dataset(file, 'r')
     array = dataset.variables[f'RRS{band}'][:]
@@ -86,6 +189,20 @@ def main():
             return
         check_neg_olci_values(args.input_path, args.output, start_date, end_date,args.region)
 
+    if args.mode == 'correct_neg_olci_values' or args.mode=='correct_neg_olci_values_slurm':
+        arguments = ['input_path', 'start_date', 'end_date', 'region']
+        if not check_required_params(arguments):
+            return
+        if not check_directory(args.input_path, False):
+            return
+        start_date, end_date = get_dates_from_arg()
+        if start_date is None or end_date is None:
+            return
+        if args.mode=='correct_neg_olci_values':
+            correct_neg_olci_values(args.input_path, start_date, end_date, args.region)
+
+        if args.mode == 'correct_neg_olci_values_slurm':
+            correct_neg_olci_values_slurm(args.input_path, start_date, end_date, args.region)
 
 def check_required_params(param_list):
     b = True
